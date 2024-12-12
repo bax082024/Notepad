548b7256172a2d6b3fd2ff05c09dd54603fa1a48
bax082024 <bax082024@gmail.com>
2024-12-12 16:13:23 +0100

    added my name

--- obj/Debug/net8.0-windows/Notepad.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Notepad.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notepad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e15b96142e4f064ab17eda75e202a0be247c45a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad1bd1f33552afafc60d75cc12ab0bb68b15cf63")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notepad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notepad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
